0752b3bd59587984946272f8fa4de8af4f1840ce
masaibar <masaibar.dev@gmail.com>
2020-03-30 22:48:42 +0900

    MainViewModelのメソッド呼び出しまで

--- app/src/main/java/com/masaibar/viewmodelsample/MainViewModel.kt
+++ app/src/main/java/com/masaibar/viewmodelsample/MainViewModel.kt
@@ -1,7 +1,14 @@
 package com.masaibar.viewmodelsample
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    init {
+        Log.d("MainViewModel", "init")
+    }
 
+    fun hello() {
+        Log.d("MainViewModel", "hello")
+    }
 }
